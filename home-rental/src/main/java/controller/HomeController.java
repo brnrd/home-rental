0151aadc029485b40f6995d3a577df8f8662e3a1
Romain FONCIER <rphonika@gmail.com>
2013-04-04 17:14:19 +0200

    Add Spring Security for authentication process.

--- home-rental/src/main/java/controller/HomeController.java
+++ home-rental/src/main/java/controller/HomeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -13,5 +14,9 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HomeController {
 
-    
+    @RequestMapping("/")
+    public ModelAndView homeView(@RequestParam(required = false, defaultValue = "") String name) {
+        ModelAndView mav = new ModelAndView("results");
+        return mav;
+    }
 }
