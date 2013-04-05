cbc14056e441f1c1d79afeaa64f8bdad1ee2544a
brnrd <bernard.debecker@gmail.com>
2013-04-05 16:41:14 +0200

    Add testUserDao

---
+++ home-rental/src/main/java/test/testUserDao.java
@@ -0,0 +1,29 @@
+package test;
+
+
+
+//import java.util.Calendar;
+//import org.springframework.context.ApplicationContext;
+//import org.springframework.context.support.ClassPathXmlApplicationContext;
+//import sun.jdbc.odbc.ee.DataSource;
+//import web.dao.impl.UserDao;
+//import web.model.User;
+
+/**
+ *
+ * @author Bernard <bernard.debecker@gmail.com>
+ */
+public final class testUserDao {
+
+//    public static void main(String[] args) {
+//        ApplicationContext ctx = new ClassPathXmlApplicationContext("../applicationContext.xml");
+//        DataSource ds = (DataSource) ctx.getBean("dataSource");
+//        UserDao dao = new UserDao();
+//        dao.setDataSource(ds);
+//        User user = new User();
+//        user.setUsername("test");
+//        user.setEmail("test@test.com");
+//        dao.create("test", "test", "test", "test", "test", Calendar.getInstance(), Boolean.TRUE);
+//        
+//    }
+}
