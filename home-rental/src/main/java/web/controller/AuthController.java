f65366e05176c387024f7807f20f37e429cbee0b
Romain FONCIER <rphonika@gmail.com>
2013-04-18 19:34:15 +0200

    Update login and signup process.

--- home-rental/src/main/java/web/controller/AuthController.java
+++ home-rental/src/main/java/web/controller/AuthController.java
@@ -12,10 +12,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.RememberMeServices;
 import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import web.model.User;
 import web.service.UserService;
 
 /**
@@ -40,26 +42,46 @@ public class AuthController {
     
     @RequestMapping(value = "/login", method = RequestMethod.GET)
     public String login() {
-        return "auth/login";
+        return "login";
     }
     
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    @ResponseBody
     public String loginProcess(@RequestParam("auth_username") String username, 
                                         @RequestParam("auth_pwd") String password,
                                         HttpServletRequest request, HttpServletResponse response) {
         
         UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
         try {
-            System.out.println("Before authenticate");
             Authentication auth = authenticationManager.authenticate(token);
-            System.out.println("After authenticate");
             SecurityContextHolder.getContext().setAuthentication(auth);
-            repository.saveContext(SecurityContextHolder.getContext(), request, response);
+            //repository.saveContext(SecurityContextHolder.getContext(), request, response);
             rememberMeServices.loginSuccess(request, response, auth);
-            return "{\"status\": \"succes\", \"msg\": \"Welcome ! You are successfully logged !\"}";
+            return "redirect:/";
         } catch (BadCredentialsException ex) {
-            return "{\"status\": \"error\", \"msg\": \"Warning ! Your login/password are incorrects !\"}";
+            return "login";
+        }
+    }
+    
+    @RequestMapping(value = "/signup", method = RequestMethod.GET)
+    public String signup() {
+        return "signup";
+    }
+    
+    @RequestMapping(value = "/signup", method = RequestMethod.POST)
+    public String signupProcess(@RequestParam("auth_username") String username, 
+                                        @RequestParam("auth_pwd") String password,
+                                        HttpServletRequest request, HttpServletResponse response) {
+        
+        // Create new player and add in context
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(username, password);
+        try {
+            Authentication auth = authenticationManager.authenticate(token);
+            SecurityContextHolder.getContext().setAuthentication(auth);
+            //repository.saveContext(SecurityContextHolder.getContext(), request, response);
+            rememberMeServices.loginSuccess(request, response, auth);
+            return "redirect:/";
+        } catch (BadCredentialsException ex) {
+            return "signup";
         }
     }
 }
--- home-rental/src/main/webapp/WEB-INF/media/js/auth.js
+++ home-rental/src/main/webapp/WEB-INF/media/js/auth.js
@@ -8,82 +8,8 @@ comment :
 */
 
 
-/* Noty
-*/
-
-
 (function() {
-  var authHandler, notifyMessage, setAjaxHeader, validForm;
-
-  notifyMessage = function(type, msg) {
-    return noty({
-      text: msg,
-      type: type,
-      dismissQueue: false,
-      timeout: 2000,
-      layout: 'topCenter',
-      theme: 'defaultTheme'
-    });
-  };
-
-  setAjaxHeader = function() {
-    return $.ajaxSetup({
-      beforeSend: function(xhr) {
-        return xhr.setRequestHeader("X-Ajax-call", "true");
-      }
-    });
-  };
-
-  authHandler = function(dataToSend, target) {
-    setAjaxHeader();
-    return $.post('/home-rental/' + target, dataToSend, function(data) {
-      $('.ajax-loader').hide();
-      data = JSON.parse(data);
-      switch (data.status) {
-        case 'success':
-          return notifyMessage('success', data.msg);
-        case 'error':
-          return notifyMessage('error', data.msg);
-      }
-    });
-  };
-
-  validForm = function(array) {
-    var i, valid;
-    valid = true;
-    i = 0;
-    while (valid && i < array.length) {
-      valid = array[i].value.length > 0;
-    }
-    return valid;
-  };
-
-  $('#signup').on("click", function(event) {
-    return $('#auth-modal-signup').modal('show');
-  });
-
-  $('#login').on("click", function(event) {
-    $('#auth-modal-login .modal-body').load('/home-rental/login .modal-body');
-    return $('#auth-modal-login').modal('show');
-  });
-
-  /* Call authHandler
-  */
 
 
-  $('#auth-process-signup').on("submit", function(event) {
-    event.preventDefault();
-    if (validForm($(this).serializeArray())) {
-
-    } else {
-      return $('#auth-modal-signup .form-error').text("Warning ! All fields must be filled !");
-    }
-  });
-
-  $('#auth-process-login').on("submit", function(event) {
-    event.preventDefault();
-    $('.ajax-loader').show();
-    return authHandler($(this).serialize(), 'login');
-  });
 
 }).call(this);
